2bcad72b461fa2d4687d959d702b59887dec7780
Sid Luthra <sidluthra@gmail.com>
2022-02-03 22:00:23 -0500

    Added interactive user interface

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
 let playerScore = 0;
 let computerScore = 0;
 
+const selections = document.querySelector("#selections");
+const result = document.querySelector("#resultStatement");
+const playerScoreDisplay = document.querySelector("#playerScore");
+const computerScoreDisplay = document.querySelector("#computerScore");
+
+const playButtons = document.querySelectorAll(".playButton");
+playButtons.forEach((button) => {
+    let move = button.id;
+    button.addEventListener('click', () => {
+        playRound(move);
+    });
+});
+
 const computerSelection = computerPlay();
 const playerSelection = "ROCK";
 
@@ -13,32 +26,36 @@ function computerPlay() {
 function playRound(playerSelection) {
     let computerSelection = computerPlay();
     if (playerSelection === computerSelection) {
-        console.log(`Computer chose ${computerSelection}. Player chose ${playerSelection}.`)
-        console.log("Tie!")
+        selections.textContent = `Computer chose ${computerSelection}. Player chose ${playerSelection}.`;
+        result.textContent = "TIE!";
     } else if (
         (playerSelection === "ROCK" && computerSelection === "SCISSORS") |
         (playerSelection === "PAPER" && computerSelection === "ROCK") |
         (playerSelection === "SCISSORS" && computerSelection === "PAPER")
     ) {
-        console.log(`Computer chose ${computerSelection}. Player chose ${playerSelection}.`)
-        console.log("Player won!")
+        selections.textContent = `Computer chose ${computerSelection}. ${playerSelection} beats ${computerSelection}.`;
+        result.textContent = "Player won!";
         playerScore ++;
-        console.log(`The score is Player: ${playerScore} | Computer ${computerScore}`)
+        updateScoreboard();
     } else {
-        console.log(`Computer chose ${computerSelection}. Player chose ${playerSelection}.`)
-        console.log("Computer won!")
+        selections.textContent = `Computer chose ${computerSelection}. ${computerSelection} beats ${playerSelection}.`;
+        result.textContent = "Computer won!";
         computerScore ++;
-        console.log(`The score is Player: ${playerScore} | Computer ${computerScore}`)
+        updateScoreboard();
     }        
 }
 
-function game() {
-    for (rounds = 1; rounds <= 5; ++rounds) {
-        playRound()
-    }
+function updateScoreboard() {
+    playerScoreDisplay.textContent = playerScore;
+    computerScoreDisplay.textContent = computerScore;
 }
 
 function reset() {
     playerScore = 0;
     computerScore = 0;
+    updateScoreboard();
+    selections.textContent = "NEW GAME!"
+    result.textContent = "Select an option below to start the game."
 }
+
+reset()
